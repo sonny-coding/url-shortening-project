262cfae371309b4466af7952a892a07e9f876710
Son Dao <udemydaohsn@gmail.com>
2023-12-15 10:57:12 -0700

    create UI frame

---
+++ client/tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          cyan: "hsl(180, 66%, 49%)",
+          "dark-violet": "hsl(257, 27%, 26%)",
+        },
+        secondary: {
+          red: "hsl(0, 87%, 67%)",
+        },
+        neutral: {
+          gray: "hsl(0, 0%, 75%)",
+          "grayish-violet": "hsl(257, 7%, 63%)",
+          "dark-blue": "hsl(255, 11%, 22%)",
+          "dark-violet": "hsl(260, 8%, 14%)",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+/* 
+colors: {
+        primary: {
+          cyan: "hsl(180, 66%, 49%)",
+          "dark-violet": "hsl(257, 27%, 26%)",
+        },
+        secondary: {
+          red: "hsl(0, 87%, 67%)",
+        },
+        neutral: {
+          gray: "hsl(0, 0%, 75%)",
+          "grayish-violet": "hsl(257, 7%, 63%)",
+          "dark-blue": "hsl(255, 11%, 22%)",
+          "dark-violet": "hsl(260, 8%, 14%)",
+        },
+      },
+*/
