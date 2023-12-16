3f64ed9dfce8c314cf68c83953b31fff33a3e824
Son Dao <udemydaohsn@gmail.com>
2023-12-15 23:55:33 -0700

    style input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
 export default function App() {
+  const handleSubmit = () => {
+    alert("submit");
+  };
   return (
-    <h1 className="text-3xl font-bold underline text-primary-cyan font-poppins">
-      Hello world!
-    </h1>
+    <div className="w-[1110px] h-[168px] flex justify-center items-center font-poppins">
+      <form onSubmit={handleSubmit} action="" className="flex gap-5">
+        <input className="" type="text" placeholder="Shorten a link here" />
+        <button
+          className="px-10 py-4 text-xl font-bold text-white rounded-md bg-primary-cyan"
+          type="submit"
+        >
+          Shorten it!
+        </button>
+      </form>
+    </div>
   );
 }
