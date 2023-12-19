850c84f7779d330be65ee46295da3535409a329f
Son Dao <udemydaohsn@gmail.com>
2023-12-19 00:32:31 -0700

    working on ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import Nav from "./sections/Nav";
 import Hero from "./sections/Hero";
 import Shorten from "./sections/Shorten";
 import Links from "./sections/Links";
 export default function App() {
-  // const handleSubmit = () => {
-  //   alert("submit");
-  // };
+  const [website, setWebsite] = useState("");
   return (
-    <div className="w-full px-2 py-3 font-poppins">
+    <div className="w-full px-2 py-3 font-poppins bg-[#EFF1F7]">
       <Nav />
       <Hero />
-      <Shorten />
+      <Shorten website={website} setWebsite={setWebsite} />
       <Links />
     </div>
   );
 }
+
+/* 
+Things to do:
+setup client - server connection
+create mongodb route
+load saved urls
+form-validation
+*/
--- client/src/sections/Links.jsx
+++ client/src/sections/Links.jsx
@@ -1,21 +1,17 @@
 import { websites } from "../constants";
 const Links = () => {
   return (
-    <div>
+    <div className="mt-4 space-y-5">
       {websites.map((each) => {
         return (
           <div
             key={each.index}
-            className="flex flex-col items-center justify-center w-full"
+            className="flex flex-col items-center justify-center w-full p-4 bg-white rounded-lg"
           >
-            {" "}
-            <div className="w-full dividy-y-2"></div>
-            <p className="text-[16px] leading-[36px] font-medium text-neutral-dark-violet">
-              {each.longUrl}
-            </p>
-            <p className="text-[16px] leading-[36px] font-medium text-primary-cyan">
-              {each.shortUrl}
-            </p>
+            <div className="flex flex-col items-center justify-center w-full divide-y-[1px] text-[16px] leading-[36px] font-medium text-center [&>*]:w-full">
+              <p className="text-neutral-dark-violet">{each.longUrl}</p>
+              <p className="text-primary-cyan">{each.shortUrl}</p>
+            </div>
             <button className="w-full py-3 text-base font-bold text-white rounded-lg bg-primary-cyan">
               Copy
             </button>
